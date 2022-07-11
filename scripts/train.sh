daaf344b8bab827e2b2c0e2113c3ee40d0ac3e44
Harish Sundaresh <yesharish@yahoo.in>
2022-07-12 08:52:51 +1000

    Working notebooks for training and inference. Pending - export inference with data classes

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,11 +4,11 @@ cd ../tools
 
 python train_net.py \
     --dataset_name          annotations \
-    --json_annotation_train /home/data/annotations/train.json \
-    --image_path_train      /home/data/annotations/ \
-    --json_annotation_val   /home/data/annotations/test.json \
-    --image_path_val        /home/data/annotations/ \
-    --num-gpus              1 \
-    --config-file           /home/layout-model-training/configs/prima/fast_rcnn_R_50_FPN_3x.yaml \
-    OUTPUT_DIR  /home/outputs/lastpage/fast_rcnn_R_50_FPN_3x/ \
+    --json_annotation_train ../data/annotations/train.json \
+    --image_path_train      ../data/annotations/ \
+    --json_annotation_val   ../data/annotations/test.json \
+    --image_path_val        ../data/annotations/ \
+    --num-gpus              1 \ # UPDATE BEFORE STARTING TRAINING
+    --config-file           ../configs/prima/fast_rcnn_R_50_FPN_3x.yaml \
+    OUTPUT_DIR  ../outputs/lastpage/fast_rcnn_R_50_FPN_3x/ \
     SOLVER.IMS_PER_BATCH 2 
