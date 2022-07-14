5772b50ff09bad31bafe1f07aa21f6cb653ce71b
Harish Sundaresh <yesharish@yahoo.in>
2022-07-14 21:33:28 +1000

    Allow running as a standalone script

--- inference.py
+++ inference.py
@@ -6,11 +6,10 @@ from numpy import NaN
 import pandas as pd
 import json
 import logging
-import warnings
 
 
 # create and configure logger
-log = logging.getLogger("lastpage-syllabus-extractor")
+log = logging.getLogger("lastpage-syllabus-parser")
 ch = logging.StreamHandler()
 log.addHandler(ch)
 formatter = logging.Formatter("%(asctime)s : %(name)s [%(levelname)s] : %(message)s")
@@ -18,11 +17,15 @@ ch.setFormatter(formatter)
 log.setLevel(logging.DEBUG)
 ch.setLevel(logging.DEBUG)
 
-# Ignore warnings (for lack of better ideas)
-log.warn("Suppressing all warnings!")
-warnings.filterwarnings("ignore", category=DeprecationWarning)
+# Redirect dependency warnings to main logger
+logging.captureWarnings(True)
+warnings_logger = logging.getLogger("py.warnings")
+log.addHandler(warnings_logger)
+warnings_logger.setLevel(logging.DEBUG)
+
 
 # Load custom layout detection model
+log.info("Loading Detectron2 layout detection model")
 model = lp.Detectron2LayoutModel(
     config_path="./outputs/lastpage/fast_rcnn_R_50_FPN_3x/config.yaml",
     model_path="./outputs/lastpage/fast_rcnn_R_50_FPN_3x/model_final.pth",
@@ -31,9 +34,10 @@ model = lp.Detectron2LayoutModel(
         0.8,
     ],
 )
+log.info("Layout detection model loading complete.")
 
 # prepare working directories
-def setup_directories():
+def setup_directories() -> Path:
     log.info("Checking data directories")
     app_dir = Path.cwd()
     data_dir = app_dir.joinpath("data")
@@ -61,6 +65,7 @@ def setup_directories():
         data_dir.mkdir()
         data_dir.joinpath("pdf").mkdir()
         data_dir.joinpath("csv").mkdir()
+    return data_dir
 
 
 def load_categories(coco_json: str) -> dict:
@@ -86,7 +91,7 @@ def load_categories(coco_json: str) -> dict:
 
 
 # Download PDF from URL
-def download_pdf(pdf_url: str, data_dir: str, ssl_verify=False) -> str:
+def download_pdf(pdf_url: str, data_dir: Path, ssl_verify=False) -> str:
     requests.packages.urllib3.disable_warnings()  # Suppress SSL warns as this is expected
     filename = pdf_url.split("/")[-1].upper().replace("%20", "")
     target_filename = data_dir.joinpath("pdf").joinpath(filename).as_posix()
@@ -110,6 +115,7 @@ def data_extractor(pdf_file_name: str) -> dict:
     pdf_tokens, pdf_images = lp.load_pdf(pdf_file_name, load_images=True)
     page_contents = {}
 
+    log.info(f"Running layout detection on {pdf_file_name}")
     for page_index in range(len(pdf_images)):  # Pages with Subject-Level Data
         syllabus_items = model.detect(pdf_images[page_index])
 
@@ -130,6 +136,7 @@ def data_extractor(pdf_file_name: str) -> dict:
             for syllabus_tokens in page_syllabus_tokens
         ]
 
+    log.info(f"Data extraction completed for {pdf_file_name}")
     return page_contents
 
 
@@ -159,9 +166,31 @@ def post_processing(page_contents: dict, categories_map: dict) -> pd.DataFrame:
     return df
 
 
-def export_data(df: pd.pd.DataFrame, csv_file_name: str, pdf_file_name: str):
+def export_data(df: pd.DataFrame, csv_file_name: str):
     app_dir = Path.cwd()
-    csv_dir = app_dir.joinpath("data").joinpath("csv").as_posix
+    csv_dir = app_dir.joinpath("data").joinpath("csv").as_posix()
     df[["page_index", "category_id", "name", "data"]].to_csv(
-        f"{csv_dir}/{pdf_file_name}.csv"
+        f"{csv_dir}/{csv_file_name}.csv"
     )
+
+
+if __name__ == "__main__":
+    URL_SOURCE = "data/urls/urls_test.txt"
+
+    data_dir = (
+        setup_directories()
+    )  # Partially redundant - annotations are expected in data dir by default
+    coco_test_ds = data_dir.joinpath("annotations").as_posix() + "/test.json"
+    categories_map = load_categories(coco_test_ds)
+    log.info(f"Reading URLs from {URL_SOURCE}")
+    with open(URL_SOURCE, encoding="utf-8") as f:
+        urls = f.readlines()
+        urls = [url.rstrip() for url in urls]
+        for url in urls:
+            downloaded_pdf_path = download_pdf(url, data_dir)
+            extracted_data = data_extractor(downloaded_pdf_path)
+            processed_data = post_processing(extracted_data, categories_map)
+            csv_filename = downloaded_pdf_path.split("/")[-1].upper()
+            export_data(processed_data, csv_filename)
+            log.info(f"Finished processing: {url}")
+    log.info("Finished processing all URLs.")
